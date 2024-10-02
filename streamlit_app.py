d7c3295be97f8d37b4f3df28a5e6b33568816172
Danya Sherbini <danyasherbini9@gmail.com>
2024-10-02 18:36:31 -0500

    prototype of simple leg tracker in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,77 @@
-import altair as alt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Sketching Leg Tracker using Streamlit
+Created on Oct 2, 2024
+@author: danyasherbini
+"""
+
 import pandas as pd
 import streamlit as st
+import os
 
-# Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+PATH = '/Users/danyasherbini/Documents/GitHub/lt-streamlit'
+os.chdir(PATH)
+
+
+# Show the page title and description
+st.set_page_config(page_title="Legislation Tracker", layout="wide") #can add page_icon argument
+st.title("Legislation Tracker")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    This app shows California bills for the 2023-2024 legislative session. 
+    Type in a search term to filter bills.
     """
 )
+
 
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("/Users/danyasherbini/Documents/GitHub/lt-streamlit/bills.csv")
     return df
-
 
 df = load_data()
 
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
+# Get only relevant columns
+keep_cols = ['bill_number','bill_name','author','coauthors','full_text','origin_chamber_id']
+bills = df[keep_cols]
+
+# Filtering widgets
 
 # Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
+#years = st.slider("Years", 1986, 2006, (2000, 2016))
 
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+# Multiselect widget for chamber
+#chambers = st.multiselect(
+#    "origin_chamber_id",
+#    [1,2],
+#    [1,2]
+#)
 
 
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
+# Search bar widget
+with st.form(key='search_form'):
+    search_term = st.text_input('Search for bills:', placeholder='Enter text to search')
+    submit_button = st.form_submit_button(label='Search')
 
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+# Filter data
+def filter_data():
+    bills_df = bills.copy()
+    df_filtered = bills_df[bills_df['bill_name'].str.contains(search_term, na=False, case=False)]
+    return df_filtered
+
+# Display filtered data when search button is clicked
+if submit_button:
+    st.dataframe(
+        filter_data(),
+        use_container_width=True,
+        hide_index=True)
+# otherwise display all data
+else:
+    st.dataframe(
+        bills.copy(),
+        use_container_width=True,
+        hide_index=True)
